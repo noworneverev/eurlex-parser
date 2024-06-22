25b4c540f650d01865e585cb9923117af81a10bd
Yan-Ying Liao <n9102125@gmail.com>
2024-06-22 14:39:54 +0200

    Support parsing for proposals

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
-import os
 
-VERSION = '0.0.3'  
+VERSION = '0.0.4'  
 DESCRIPTION  = 'Eurlex parser for fetching and parsing Eurlex data.'
 
 def read_requirements():
